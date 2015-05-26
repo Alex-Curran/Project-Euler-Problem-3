a7f977af040e4ff2c7f285eadf51e5dbb4a49ea4
Alex-Curran <alexcurran839@gmail.com>
2015-05-25 20:42:18 -0500

    Set up

---
+++ Problem-3/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ *   Project Euler: Problem 3
+ *   Alex Curran 
+ *   
+ *   The prime factors of 13195 are 5, 7, 13 and 29.
+ *   What is the largest prime factor of the number 600851475143 ?
+ *   
+ *  https://projecteuler.net/problem=3
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
